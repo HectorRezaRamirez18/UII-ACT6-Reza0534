1fbef39616953db41412829ddb8367f07a33578b
HECTOR_EZEQUIEL_REZA_RAMIREZ <a.21308051280534@cbtis128.edu.mx>
2024-04-30 13:28:24 +0000

    ac

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,198 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      title: 'Pizzeria',
+      home: MyHomePage(title: 'Pizzeria'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        body: PageView(
+      children: [
+        Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset("images/xdlol1.jpg", fit: BoxFit.cover),
+            ),
+            const Center(
+              child: SizedBox(
+                width: 200,
+                height: 300,
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Cliente1",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                        Divider(
+                          thickness: 2,
+                          height: 25,
+                        ),
+                        Icon(Icons.arrow_circle_right),
+                        Text("Trabajador 1"),
+                        Text("Calle Nv Zelanda"),
+                        Text("656-1893"),
+                        Text("Tamaño Grande"),
+                        Text("Peperoni,aceitunas"),
+                        Text("A nombre de Jose"),
+                        Text("Paquete Familiar"),
+                        Text("Efectivo")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
-      ),
-    );
+        Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset("images/xdlol2.jpg", fit: BoxFit.cover),
+            ),
+            const Center(
+              child: SizedBox(
+                width: 200,
+                height: 300,
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Cliente 2",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                        Divider(
+                          thickness: 2,
+                          height: 25,
+                        ),
+                        Icon(Icons.drive_eta),
+                        Text("Trabajador 2"),
+                        Text("Calle FincaBonita"),
+                        Text("656-4590"),
+                        Text("Tamaño chica"),
+                        Text("Peperoni,piña"),
+                        Text("A nombre de Lupe"),
+                        Text("Paquete Individual"),
+                        Text("Tarjeta")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset("images/xdlol3.jpg", fit: BoxFit.cover),
+            ),
+            const Center(
+              child: SizedBox(
+                width: 200,
+                height: 300,
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Cliente 3",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                        Divider(
+                          thickness: 2,
+                          height: 25,
+                        ),
+                        Icon(Icons.drive_eta),
+                        Text("Trabajador 3"),
+                        Text("Calle portillo"),
+                        Text("656-2376"),
+                        Text("Tamaño mediana"),
+                        Text("jamon,carne"),
+                        Text("A nombre de Luis"),
+                        Text("Paquete amigos"),
+                        Text("Efectivo")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+        Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height,
+              width: MediaQuery.of(context).size.width,
+              child: Image.asset("images/xdlol4.jpg", fit: BoxFit.cover),
+            ),
+            const Center(
+              child: SizedBox(
+                width: 200,
+                height: 300,
+                child: Card(
+                  child: Padding(
+                    padding: EdgeInsets.all(20),
+                    child: Column(
+                      children: [
+                        Text(
+                          "Cliente 4",
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w600),
+                        ),
+                        Divider(
+                          thickness: 2,
+                          height: 25,
+                        ),
+                        Icon(Icons.drive_eta),
+                        Text("Trabajador 4"),
+                        Text("Calle Granjero"),
+                        Text("656-6525"),
+                        Text("Tamaño Grande"),
+                        Text("Carne,piña"),
+                        Text("A nombre de Ezequiel"),
+                        Text("Paquete Individual"),
+                        Text("Tarjeta")
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ],
+    ));
   }
 }
